1394f55ae60fbca53a927334ebc69d9e6338f687
Alan Vera Alba <ala.veraa@alumnos.duoc.cl>
2019-08-06 23:56:44 -0400

    Primer Guardado/Commit

---
+++ index.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('home.html')
+
+@app.route('/About')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) 
